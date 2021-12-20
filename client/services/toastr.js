f10c00327676c3587f77f86189c916d0d52cb4b3
Stone Tech <jsreputation@gmail.com>
2021-12-21 01:23:56 +0900

    Tweak: abi import and toastr service prototype

---
+++ client/services/toastr.js
@@ -0,0 +1,12 @@
+import toast from 'react-hot-toast';
+
+export default function Toastr() {
+    let options = {
+        position: 'top-center',
+        autoClose: 3500,
+        closeOnClick: true,
+        icon: '👏'
+    }
+
+    
+}
